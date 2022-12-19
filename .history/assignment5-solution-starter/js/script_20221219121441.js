f1e3e6c2c6a3f9d07bf9bf29a221abffe44fcb29
Normandy72 <99590600+Normandy72@users.noreply.github.com>
2022-12-19 12:20:08 +0300

    feat: add random choosing of categories

--- .history/assignment5-solution-starter/js/script_20221219121441.js
+++ .history/assignment5-solution-starter/js/script_20221219121441.js
@@ -99,7 +99,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      var chosenCategoryShortName = function(categories){
+        
+      };
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -113,13 +115,13 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+      // var homeHtmlToInsertIntoMainPage = ....
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      // ....
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
